1094532f28da40cef8f1e473b98e553867f98d9b
daviddangtim <132074594+daviddangtim@users.noreply.github.com>
2023-05-19 01:51:14 +0100

    Dangtim: Added the Bank Account Class

---
+++ src/account/BankAccount.java
@@ -0,0 +1,41 @@
+package account;
+public class BankAccount {
+    private String accountNumber;
+    private double balance;
+
+    public BankAccount(String accountNumber, double initialBalance) {
+        this.accountNumber = accountNumber;
+        this.balance = initialBalance;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+            System.out.println("Deposited: $" + amount);
+        } else {
+            System.out.println("Invalid amount. Please deposit a positive value.");
+        }
+    }
+
+    public void withdraw(double amount) {
+        if (amount > 0) {
+            if (balance >= amount) {
+                balance -= amount;
+                System.out.println("Withdrawn: $" + amount);
+            } else {
+                System.out.println("Insufficient funds. Unable to withdraw $" + amount);
+            }
+        } else {
+            System.out.println("Invalid amount. Please withdraw a positive value.");
+        }
+    }
+}
+
